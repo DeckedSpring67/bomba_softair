5c90fc0baa53c9af6653393c823c9c352619dafe
Marco Martini <deckedspring@gmail.com>
2020-07-25 19:09:48 +0100

    Cleanup and first buzzer code

--- bomba.c
+++ bomba.c
@@ -7,19 +7,49 @@
 #include <pthread.h>
 #include <wiringPi.h>
 #include <limits.h>
+#include <softTone.h>
+#define BUZZER 0
 
-//Gloabal button thingy
-u_int8_t buttonPressed = 0;
-time_t timeButtonPressed;
+#define DO 523
+#define REb 554
+#define RE 587
+#define MIb 622
+#define MI 659
+#define FA 698 
+#define SOLb 739
+#define SOL 783
+#define LAb 830
+#define LA 880
+#define SIb 932
+#define SI 987
+#define DO8 1046
 
+
+int nge[] = {DO,MIb,FA,MIb,RE,FA,FA,SIb,LAb,SOL,FA,SOL,SOL,SIb,
+	DO8,FA,MIb,SIb,SIb,SOL,SIb,SIb,DO8};
+
+int nge_beat[] = {2,2,2,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,1,1,1,2};
 
 void handleInterrupt(){
 	printf("Premuto pulsante\n");
 	fflush(stdout);
 }
 
-void *refresh_display(void *args){
-	return NULL;
+void standbyBuzzer(){
+	pinMode(BUZZER,OUTPUT);
+	delay(10);
+	digitalWrite(BUZZER,HIGH);
+}
+
+void setupTimer(){
+}
+
+int activeBuzzer(){
+	if(softToneCreate(BUZZER) == -1){
+		printf("Soft Tone Failed \n");
+		return 1;
+	}
+	return 0;
 }
 
 int main() {
@@ -27,74 +57,32 @@ int main() {
 	wiringPiSetup();
 	//Interrupt for button
 	wiringPiISR(27,INT_EDGE_RISING,&handleInterrupt);
-	
 	printf("Bomba Partita\n");
+	standbyBuzzer();
+	activeBuzzer();
+	int i = 0;
+	for(i = 0; i < sizeof(nge)/4; i++){
+		softToneWrite(BUZZER,nge[i]);
+		delay(nge_beat[i] * 250);
+		softToneWrite(BUZZER,0);
+		delay(25);
+	}
+	standbyBuzzer();
+	delay(1000);
+	activeBuzzer();
+	for(i = 0; i < sizeof(nge)/4; i++){
+		softToneWrite(BUZZER,nge[i]);
+		delay(nge_beat[i] * 250);
+		softToneWrite(BUZZER,0);
+		delay(25);
+	}
+	standbyBuzzer();
 	ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
 	ssd1306_clearDisplay();
-	
-	//Create thread ids
-	pthread_t refresh_id; 
-	pthread_t hightemps_id;
-	pthread_t ac_id;
-	pthread_t amb_id;
-	pthread_t lowtemps_id;
-
-	temps.timeLastSent = 0;
-
-	//Add a slight delay after the first reading to prevent wrong ones
-	pthread_create(&ac_id, NULL, getAcTemp, &temps);
-	delay(1000);
-
-	//create other variables
-	time_t timeNow;
-	double timeDiff;
-	char text[300];
-	time_t timeNowLog;
-	timeLastLog = 0;
-
-	while(1){
-		if(!buttonPressed){
-			pthread_create(&ac_id, NULL, getAcTemp, &temps);
-			pthread_create(&amb_id, NULL, getAmbTemp, &temps);
-			
-			//Detach threads to avoid memory leak
-			pthread_join(ac_id,NULL);
-			pthread_join(amb_id,NULL);
-
-			pthread_create(&refresh_id, NULL, refresh_display, &temps);
-			pthread_detach(refresh_id);
-
-				if(temps.ac > temps.maxAC || temps.amb > temps.maxAMB){	
-					pthread_create(&hightemps_id, NULL, handle_HighTemp, &temps);
-				}
-				else if(temps.ac < (temps.maxAC - 1) && temps.amb < (temps.maxAMB - 1)){
-					pthread_create(&lowtemps_id, NULL, handle_LowTemp, &temps);
-				}
-			pthread_detach(lowtemps_id);
-			pthread_detach(hightemps_id);
-		}
-		else{
-			time(&(timeNow));
-			timeDiff = difftime(timeNow,timeButtonPressed);
-			//When button has been pressed pause the program for 15 minutes
-			// and show it on screen
-			if(timeDiff < 900){
-				sprintf(text,"Sistema in pausa\n%.0f sec. al riavvio\nPremere RESET per \nriavvio manuale\n",900-timeDiff);
-				ssd1306_setTextSize(1);
-				ssd1306_drawString(text);
-				ssd1306_display();
-				ssd1306_clearDisplay();
-			}
-			else{
-				buttonPressed = 0;
-			}
-		}
-		char logbody[400];
-		time(&(timeNowLog));
-	       	sprintf(logbody,"%f;%f;%s",temps.ac,temps.amb,temps.temp_alert);
-		logPrintf(logbody,timeNowLog);
-		delay(500);
-	}
+	ssd1306_setTextSize(1);
+	ssd1306_drawString("Bomba inizializzata");
+	ssd1306_display();
+	ssd1306_clearDisplay();
 	return 0;
 }
 
